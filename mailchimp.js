ca976b22500029f7f7899a52f93efa128ad93f2b
Serik <serik@Seriks-MacBook-Air.local>
2023-06-23 02:28:16 +0600

    tag

--- mailchimp.js
+++ mailchimp.js
@@ -8,39 +8,34 @@ import jsonp from "./jsonp.js";
  * @link https://cdn.jsdelivr.net/gh/serikshaikamalov/mailchimp-subscribe-form@main/mailchimp.js
  */
 export const subscribe = async (event, _url) => {
-  try {
-    event.preventDefault();
+  event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const formProps = Object.fromEntries(formData);
-    console.log("formProps: ", formProps);
+  const formData = new FormData(event.target);
+  const formProps = Object.fromEntries(formData);
+  console.log("formProps: ", formProps);
 
-    if (!formProps?.email)
-      throw new Error("Please provide email for subscription");
+  if (!formProps?.email)
+    throw new Error("Please provide email for subscription");
 
-    const { email } = formProps;
-    const url =
-      _url ||
-      `https://itsnuqtah.us20.list-manage.com/subscribe/post-json?u=1d845a4ca14e641f9ea2e18ef&id=03e21cfcd1&f_id=009856e6f0&EMAIL=${email}`;
+  const { email } = formProps;
+  const url =
+    _url ||
+    `https://itsnuqtah.us20.list-manage.com/subscribe/post-json?u=1d845a4ca14e641f9ea2e18ef&id=03e21cfcd1&f_id=009856e6f0&EMAIL=${email}`;
 
-    return jsonp(
-      url,
-      {
-        param: "c",
-      },
-      function (err, data) {
-        if (err) {
-          throw new Error(err);
-        } else if (data.result !== "success") {
-          console.log("Mailchimp > ex:", data.msg);
-          throw new Error(data.msg);
-        } else {
-          return data.msg;
-        }
+  return jsonp(
+    url,
+    {
+      param: "c",
+    },
+    function (err, data) {
+      if (err) {
+        throw new Error(err);
+      } else if (data.result !== "success") {
+        console.log("Mailchimp > ex:", data.msg);
+        throw new Error(data.msg);
+      } else {
+        return data.msg;
       }
-    );
-  } catch (ex) {
-    console.error(`Mailchimp > execption on subscribing: `, ex);
-    throw ex;
-  }
+    }
+  );
 };
