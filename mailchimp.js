fd8520b508dd367e546201d1ec17c8bd052a9c29
Serik <serik@Seriks-MacBook-Air.local>
2023-06-23 00:38:35 +0600

    subscribe

---
+++ mailchimp.js
@@ -0,0 +1,36 @@
+/**
+ * Used to subscribe to the Mailchimp newsletter
+ * @param {*} event - an event of onsubmit handler
+ * @param {*} _url - mailchimp provided url. You can get it from admin dashboard
+ */
+const subscribe = async (event, _url) => {
+  try {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formProps = Object.fromEntries(formData);
+    console.log("formProps: ", formProps);
+
+    if (!formProps?.email)
+      throw new Error("Please provide email for subscription");
+
+    const { email } = formProps;
+    const url =
+      _url ||
+      `https://itsnuqtah.us20.list-manage.com/subscribe/post-json?u=1d845a4ca14e641f9ea2e18ef&id=03e21cfcd1&f_id=009856e6f0&EMAIL=${email}`;
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    if (response.ok) {
+      const responseInJson = await response.json();
+      console.log("Mailchimp > subscribe: ", responseInJson);
+      return responseInJson;
+    } else {
+      throw new Error(response.text);
+    }
+  } catch (ex) {
+    console.error(`Mailchimp > execption on subscribing: `, ex);
+    throw ex;
+  }
+};
