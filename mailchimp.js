947160eb756e13045a3bb9ad9d527d0b78a9e729
Serik <serik@Seriks-MacBook-Air.local>
2023-06-23 02:02:16 +0600

    update path

--- mailchimp.js
+++ mailchimp.js
@@ -1,4 +1,4 @@
-import jsonp from "https://cdn.jsdelivr.net/gh/webmodules/jsonp@0.2.1/index.js";
+import jsonp from "https://cdn.jsdelivr.net/npm/jsonp@0.2.1/index.min.js";
 
 /**
  * Used to subscribe to the Mailchimp newsletter
